04f1cd628b34c3e139b3e1efb82657e45979c0e3
Henrique Guimaraes <henrique.guimaraes@dommus.com.br>
2021-07-02 20:43:07 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
+
+export function App() {
+
+    // throw new Error("Deu um pau doido aqui")
+
+    return <RepositoryList />
+}
